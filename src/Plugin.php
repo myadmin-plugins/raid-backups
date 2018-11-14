c515ae97e0c3e6f38fabd1b330b7a41b67213f1e
Joe Huss <detain@interserver.net>
2018-11-13 22:01:15 -0500

    localization update

--- src/Plugin.php
+++ src/Plugin.php
@@ -42,9 +42,8 @@ class Plugin
 		$menu = $event->getSubject();
 		if ($GLOBALS['tf']->ima == 'admin') {
 			function_requirements('has_acl');
-			if (has_acl('client_billing')) {
-				$menu->add_link('admin', 'choice=none.abuse_admin', '/lib/webhostinghub-glyphs-icons/icons/development-16/Black/icon-spam.png', __('Raid'));
-			}
+            if (has_acl('client_billing')) {
+            }
 		}
 	}
 
@@ -72,7 +71,5 @@ class Plugin
          * @var \MyAdmin\Settings $settings
          **/
         $settings = $event->getSubject();
-		$settings->add_text_setting(__('General'), __('Raid'), 'abuse_imap_user', __('Raid IMAP User'), __('Raid IMAP Username'), ABUSE_IMAP_USER);
-		$settings->add_text_setting(__('General'), __('Raid'), 'abuse_imap_pass', __('Raid IMAP Pass'), __('Raid IMAP Password'), ABUSE_IMAP_PASS);
-	}
+    }
 }
